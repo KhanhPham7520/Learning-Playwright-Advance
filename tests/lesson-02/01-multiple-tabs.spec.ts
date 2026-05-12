14263f5f207a274d66ee8d19c76820bf91c6532e
Khanh Pham Phan Nhat <phamphannhatkhanh7520@gmail.com>
2026-05-13 01:06:55 +0700

    API Testing Using Playwright

--- tests/lesson-02/01-multiple-tabs.spec.ts
+++ tests/lesson-02/01-multiple-tabs.spec.ts
@@ -1,4 +1,7 @@
 import { test, expect } from "@playwright/test";
+import { log } from "node:console";
+import { request } from "node:http";
+import { title } from "node:process";
 
 test.describe("Example tests", async () => {
     test("Has title", async ({ context, browser }) => {
@@ -50,6 +53,84 @@ test.describe("Example tests", async () => {
         });
     });
 
+    // PUT - cập nhật toàn bộ
+    test('PUT /todos.php — Cập nhật toàn bộ todo', async ({ request }) => {
+        const testData = {
+            bodyCreated: {
+                title: 'Học Playwright API Testing By Khanh',
+                status: 'pending',
+                priority: 'high',
+            },
+            bodyUpdated: {
+                title: "New title - Modified from Khanh",
+                status: "completed"
+            }
+        }
 
+        let createdId: number = 0;
+
+        // Step 1
+        await test.step("Step 1 : Tạo todo", async () => {
+            const bodyCreate = testData.bodyCreated;
+            const response = await request.post('https://material.playwrightvn.com/api/todo-app/v1/todo.php', {
+                data: bodyCreate
+            });
+
+            // Verify json trả về 201 sau khi tạo
+            expect(response.status()).toBe(201);
+
+            const body = await response.json();
+            createdId = body.todo.id;
+
+            console.log("Created id: ", createdId);
+        })
+
+
+        // Step 2
+        await test.step("Step 2 : update todo", async () => {
+            const bodyUpdated = testData.bodyUpdated;
+
+            const response = await request.put('https://material.playwrightvn.com/api/todo-app/v1/todo.php', {
+                data: {
+                    ...bodyUpdated,
+                    id: createdId
+                } // spread operator
+            })
+
+            expect(response.status()).toBe(200);
+            const body = await response.json();
+
+            expect(body.todo.title).toBe(bodyUpdated.title)
+            expect(body.todo.status).toBe(bodyUpdated.status)
+        });
+
+        await test.step("Step 3 : Xóa todo đã tạo ra", async () => {
+            // Xóa todo
+            let response = await request.delete('https://material.playwrightvn.com/api/todo-app/v1/todo.php', {
+                data: {
+                    id: createdId
+                }
+            })
+            expect(response.status()).toBe(200);
+
+            // Get lai todo
+            response = await request.get('https://material.playwrightvn.com/api/todo-app/v1/todos.php', {
+                params: {
+                    id: createdId
+                },
+            });
+            expect(response.status()).toBe(404);
+        })
+    });
+
+    test("Test profile endpoint - Get Profile success", async ({ request }) => {
+        await test.step("Get profile information", async () => {
+            const response = await request.get("https://material.playwrightvn.com/api/user-management/v1/login.php", {
+                headers: {
+                    //Authorizationn: `Bearer: ${token}`
+                }
+            })
+        })
+    })
 
 })
